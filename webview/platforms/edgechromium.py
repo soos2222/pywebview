675dc92f9b970a4e08288b958e3b3e9ed21843ca
soos2222 <soos2222@naver.com>
2023-01-06 11:50:52 +0900

    on mouse click

--- webview/platforms/edgechromium.py
+++ webview/platforms/edgechromium.py
@@ -58,6 +58,7 @@ class EdgeChrome:
         self.web_view.NavigationStarting += self.on_navigation_start
         self.web_view.NavigationCompleted += self.on_navigation_completed
         self.web_view.WebMessageReceived += self.on_script_notify
+        self.web_veiw.OnMouseClick += self.on_mouse_click
         print(dir(self.web_view.CoreWebView2))
 
         if window.transparent:
@@ -161,6 +162,9 @@ class EdgeChrome:
 
     def on_navigation_start(self, sender, args):
         pass
+    
+    def on_mouse_click(self):
+        print('clicked')
 
     def on_navigation_completed(self, sender, args):
         url = str(sender.Source)
